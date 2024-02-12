6bb051f46d68ed4377babb58d58ac64b4570e83f
João Felipe de Lima <joao.lima@levva.io>
2024-02-12 17:49:22 -0300

    feature: speech recognition API

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -5,19 +5,24 @@ import { X } from "lucide-react";
 
 interface NoteCardProps {
   note: {
+    id: string;
     date: Date;
     content: string;
   };
+  oneNoteDeleted: (id: string) => void;
 }
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, oneNoteDeleted }: NoteCardProps) {
   return (
     // Dialog.Root é o componente que deve ficar em volta do componente de modal
     <Dialog.Root>
       {/* Dialog.Trigger é o componente que abre o modal */}
       <Dialog.Trigger className="rounded-md outline-none text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-small font-medium text-slate-300">
-          {note.date.toISOString()}
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className="text-small leading-6 text-slate-400">{note.content}</p>
 
@@ -29,7 +34,7 @@ export function NoteCard({ note }: NoteCardProps) {
         {/* Dialog.Overlay é o componente que renderiza o fundo com opacidade */}
         <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
         {/* Dialog.Content é o componente que renderiza o conteúdo da modal */}
-        <Dialog.Content className="z-10 fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className="z-10 fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
@@ -47,6 +52,7 @@ export function NoteCard({ note }: NoteCardProps) {
           </div>
 
           <button
+            onClick={() => oneNoteDeleted(note.id)}
             type="button"
             className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
           >
